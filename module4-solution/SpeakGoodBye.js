b5ba1b63b88e8c014e19fa2b7fe646ab415b7b57
Bryan Mensah <44969069+bryanm17078@users.noreply.github.com>
2023-03-13 09:16:29 +0000

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -23,3 +23,15 @@ function speak(name) {
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
+
+
+(function(window) {
+	var speakWord = "Good Bye";
+
+	var byeSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
